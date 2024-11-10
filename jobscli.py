3849b3260b4dcb168470ffd0827c5e491b72e9a3
a106833 <a106833@uminho.pt>
2024-11-10 13:38:04 +0000

    Update jobscli.py

--- jobscli.py
+++ jobscli.py
@@ -1,17 +1,17 @@
 import typer
 import requests #pede acesso ao api
+import re
 
 API_KEY = '71c6f8366ef375e8b61b33a56a2ce9d9'
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36', #engana o api a pensar que estou a aceder pro um navegador
-    'api_key': f"{API_KEY}"
 }
 
-def response(): #função para fazer a requisição
-    url = 'https://api.itjobs.pt/job/list.json'
+def response(page): #função para fazer a requisição
+    url = f'https://api.itjobs.pt/job/list.json?api_key={API_KEY}&page={page}'
     response = requests.get(url, headers=headers)
-    print(response.status_code)
-    return response.json()
+    data = response.json()
+    return data
 
 app = typer.Typer()
 
@@ -19,7 +19,6 @@ app = typer.Typer()
 def top(n: int):
     """ Lista os N trabalhos mais recentes publicados pela itjobs.pt"""
     #argumento opcional para csv
-    
 
 @app.command()
 def search(n: int):
@@ -38,8 +37,36 @@ def skills(n:int):
     #recebe a lista de skills e datas de início e de fim [skill1, skill2, skill3] dataInicial dataFinal
     #argumento opcional para csv
 
-#para cada funcionalidade (exceto salary), deve poder exportar para CSV a informação com os campos: título, empresa, descrição, data de publicação, salário e localização
-#para isso, é necessário poder adicionar um argumento opcional a cada um dos comandos
+@app.command()
+def contacto(job_id:int):
+    """ Extrai o número de telefone mencionado numa vaga. """
+    page = 1 
+    phones = None 
+    while True: 
+        data = response(page) 
+        if 'results' not in data or not data['results']: 
+            phones = None 
+            break
+        job = None 
+        for x in data['results']: 
+            if x['id'] == job_id: 
+                job = x 
+                break 
+        if job: 
+            company = job.get('company', {}) 
+            phones = company.get('phone') 
+            if not phones: 
+                body = job.get("body", None) 
+                phones = re.findall(r"\b((\+351)?(9|2)\d{2}\s?\d{3}\s?\d{3})\b", body) 
+                if not phones:
+                    description = company.get('description', None)
+                    phones = re.findall(r"\b((\+351)?(9|2)\d{2}\s?\d{3}\s?\d{3})\b", description)
+            break
+        page += 1 
+    if phones: 
+        print(f"Telefones disponíveis: {phones}") 
+    else:
+        print("Nenhum número de telefone especificado.")
 
 if __name__ == "__main__":
     app()
