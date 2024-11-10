ac12b96fec07e00d634fd9d1fced2d8773f0c7e1
a106833 <a106833@uminho.pt>
2024-11-10 13:30:32 +0000

    Update jobscli.py

--- jobscli.py
+++ jobscli.py
@@ -1,17 +1,16 @@
 import typer
-import requests #pede acesso ao api
+import requests
+import re
 
 API_KEY = '71c6f8366ef375e8b61b33a56a2ce9d9'
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36', #engana o api a pensar que estou a aceder pro um navegador
-    'api_key': f"{API_KEY}"
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36', #engana o api a pensar que estou a aceder pro um navegador
 }
 
-def response(): #função para fazer a requisição
-    url = 'https://api.itjobs.pt/job/list.json'
+def response(page): #função para fazer a requisição
+    url = f'https://api.itjobs.pt/job/list.json?api_key={API_KEY}&page={page}'
     response = requests.get(url, headers=headers)
-    print(response.status_code)
-    return response.json()
+    data = response.json()
+    return data
 
 app = typer.Typer()
 
@@ -19,7 +18,6 @@ app = typer.Typer()
 def top(n: int):
     """ Lista os N trabalhos mais recentes publicados pela itjobs.pt"""
     #argumento opcional para csv
-    
 
 @app.command()
 def search(n: int):
@@ -28,9 +26,34 @@ def search(n: int):
     #argumento opcional para csv
 
 @app.command()
-def salary(n: int):
-    """ Extrai a informação relativa ao salário oferecido por uma determinado job id"""
-    #mesmo que o valor seja 'wage a null'; neste caso usar expressões regulares para procurar noutros campos relevantes
+def salary(job_id: int):
+    """Extrai o salário de uma vaga pelo job_id."""
+    page = 1
+    while True:
+        data = response(page)
+        if 'results' not in data or not data['results']:
+            print(f"Job com ID {job_id} não encontrado.")
+            break
+        job = None
+        for job in data['results']:
+            if job['id'] == job_id:
+                break
+        else:
+            job = None
+        if job:
+            wage = job.get("wage")
+            if wage:
+                print(f"Salário: {wage}")
+            else:
+                body = job.get("body", "")
+                wage_match = re.search(r"(\d{3,}([.,]\d{3})?\s?(€|\$|USD|£|₹))", body)
+                if wage_match:
+                    estimated_wage = wage_match.group(0) #group retorna as partes da string que correspondem ao padrão da repex
+                    print(f"Salário: {estimated_wage}") #0 é o índice da correspondência
+                else:
+                    print("Salário não especificado")
+            break
+        page += 1
 
 @app.command()
 def skills(n:int):
