3c01344bed2143c0f6222879a31e5194af597a81
Maria Inês Magalhães de Castro <a106828@uminho.pt>
2024-12-30 18:52:13 +0000

    Update jobscli.py

--- jobscli.py
+++ jobscli.py
@@ -10,7 +10,7 @@ from bs4 import BeautifulSoup
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,/;q=0.8',
     'Accept-Language': 'en-US,en;q=0.5',
     'Accept-Encoding': 'gzip, deflate',
     'Connection': 'keep-alive',
@@ -67,11 +67,11 @@ def list_skills(job_title: str, export_csv: bool = False):
         skills = get_skills_from_job(url)
         all_skills.extend(skills)
     skill_count = Counter(all_skills)
-    sorted_skills = sorted(skill_count.items(), key=lambda x: x[1], reverse=True) #ordena as skills por ordem crescente de frequência
-    skills = [{"skill": skill, "count": count} for skill, count in sorted_skills]
+    top_skills = skill_count.most_common(10)
+    skills = [{"skill": skill, "count": count} for skill, count in top_skills] #ordena as skills por ordem crescente de frequência
     print(json.dumps(skills, indent=4))
     if export_csv:
         exportar_csv(skills, filename=f'lista_skills_{job_title}.csv')
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     app()
